290a6e37d66493feb2f26d564a7acf3d3b350acc
ileztom <91593668+ileztom@users.noreply.github.com>
2025-04-15 23:02:46 +0300

    Update test_add_camera_to_cart.py

--- tests/test_add_camera_to_cart.py
+++ tests/test_add_camera_to_cart.py
@@ -4,4 +4,5 @@ from main_page import MainPage
 
 def test_add_camera_to_cart(driver):
     # 1. Открываем главную страницу
-    main_page.driver.get("https://demo.opencart.com/")
+    main_page = MainPage(driver)
+    
