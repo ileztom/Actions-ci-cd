da7f8fc91654b76e329a4860ae490c0fedbb2ca5
ileztom <91593668+ileztom@users.noreply.github.com>
2025-04-15 22:58:37 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import allure
 import pytest
 import logging
+import tempfile
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
